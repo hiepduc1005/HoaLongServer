43317cdd77c5c0ed3c067f05ecd25341eeb8bbb3
duc hiep <hiep100524@gmail.com>
2024-05-06 22:32:28 +0700

    update

--- src/main/java/com/hstore/vn/dto/request/ProductRequest.java
+++ src/main/java/com/hstore/vn/dto/request/ProductRequest.java
@@ -3,7 +3,7 @@ package com.hstore.vn.dto.request;
 import java.math.BigDecimal;
 
 public class ProductRequest {
-	
+
 	public String name;
 	public BigDecimal price;
 	public String description;
@@ -12,36 +12,46 @@ public class ProductRequest {
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public BigDecimal getPrice() {
 		return price;
 	}
+
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public byte[] getImgData() {
 		return imgData;
 	}
+
 	public void setImgData(byte[] imgData) {
 		this.imgData = imgData;
 	}
-	public ProductRequest( String name, BigDecimal price, String description,
+
+	public ProductRequest(String name, BigDecimal price, String description,
 			byte[] imgData) {
-				
+
 		this.name = name;
 		this.price = price;
 		this.description = description;
 		this.imgData = imgData;
 	}
-	
-	
-	
+
+	public ProductRequest() {
+
+	}
+
 }
