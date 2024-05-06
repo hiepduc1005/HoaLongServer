5a4bf212d451287cd9e427d2b7584baf9743d505
duc hiep <hiep100524@gmail.com>
2024-05-07 02:18:30 +0700

    update new

--- src/main/java/com/hstore/vn/controller/CartController.java
+++ src/main/java/com/hstore/vn/controller/CartController.java
@@ -70,10 +70,10 @@ public class CartController {
 
 		Cookie cookie = new Cookie("cart", newCartCookie);
 		cookie.setMaxAge(60 * 60);
-		cookie.setHttpOnly(true);
+		cookie.setDomain(".netlify.app");
+		// cookie.setHttpOnly(true);
 		cookie.setSecure(true);
-		// cookie.setPath("/");
-
+		cookie.setPath("/");
 		response.addCookie(cookie);
 
 		return new ResponseEntity<String>("Product add to cart successfully!", HttpStatus.OK);
@@ -95,10 +95,10 @@ public class CartController {
 
 		Cookie cookie = new Cookie("cart", newCartCookie);
 		cookie.setMaxAge(60 * 60);
-		// cookie.setDomain("hoalong.netlify.app");
-		cookie.setHttpOnly(true);
+		cookie.setDomain(".netlify.app");
+		// cookie.setHttpOnly(true);
 		cookie.setSecure(true);
-		// cookie.setPath("/");
+		cookie.setPath("/");
 
 		response.addCookie(cookie);
 
